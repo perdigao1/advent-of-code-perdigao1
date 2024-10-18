c63cb210dabf2cdda45b789d19aa78d9c35abdf3
Luis Perdigao <perdigao1@yahoo.com>
2024-10-18 18:23:51 +0100

    day14  part2

--- AdventOfCode2020Cpp/VS/AdventOfCode2020/day14/day14.cpp
+++ AdventOfCode2020Cpp/VS/AdventOfCode2020/day14/day14.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #include <cassert>
 
 #include <numeric> // for std::gcd, make sure C++ version >=17
+#include <bitset>
 
 
 //Implement current mask as a singleton class
@@ -72,10 +73,97 @@ public:
     }
 
 };
-
 // Initialize pointer to zero so that it can be initialized in first call to getInstance
 Mask* Mask::instance= nullptr;
 
+
+
+class Mask2 {
+private:
+    static Mask2* instance;
+    Mask2() {}  // Private constructor
+
+    vector<int> zeros_idxs; //indicates indices where 0's are
+    vector<int> ones_idxs;
+    vector<int> x_idxs;
+
+public:
+    Mask2(const Mask2&) = delete;  // Delete copy constructor
+    Mask2& operator=(const Mask2&) = delete;  // Delete assignment operator
+
+    static Mask2* getInstance() {
+        if (instance == nullptr) {
+            instance = new Mask2();
+        }
+        return instance;
+    }
+
+    //Functions
+    void setMask2(const string& mask_str) {
+        x_idxs.clear();
+        ones_idxs.clear();
+        zeros_idxs.clear();
+
+        for (int i = 0; i<36 ; i++) {
+            auto c = mask_str[35-i]; //reverse
+            switch (c) {
+            case 'X':
+                x_idxs.push_back(i);
+                break;
+            case '1':
+                ones_idxs.push_back(i);
+                break;
+            case '0':
+                zeros_idxs.push_back(i);
+                break;
+            default:
+                cerr << "Can't interpret character " << c << " at " << i << endl;
+            }
+        }
+        cout << "New mask created" << endl;
+    }
+
+    vector<uintll> applyMaskTo(uintll value) {
+        // Gets all the possible masked values
+        
+        //Get size of the binary number needed to do all the Xs
+        cout << "applyMaskTo : " << value << endl;
+
+        bitset<36> b_choices_base(value);
+
+        for (auto iones_loc : ones_idxs) {
+            b_choices_base.set(iones_loc); //set to 1
+        }
+        //Zeros are unchanged
+        //for (auto zeros_loc : zeros_idxs) {
+        //    b_choices_base.reset(zeros_loc); //set to zero
+        //}
+
+        uintll n_choices = pow(2ll, x_idxs.size());
+
+        vector<uintll> allvalues;
+
+        for (uintll i = 0; i < n_choices; i++) {
+            
+            auto b_choice_cpy = b_choices_base; //copy
+
+            bitset<36> b_choice_rep(i);
+
+            //Loop for adjusting the binary representation, from last index
+            for (int j = 0; j<x_idxs.size(); j++) {
+                auto b_digit = b_choice_rep[j];
+                auto idx0 = x_idxs[j];
+                b_choice_cpy.set(idx0, b_digit); 
+            }
+            auto value = b_choice_cpy.to_ullong();
+            allvalues.push_back(value);
+            //cout << "new value: " << b_choice_cpy.to_ullong() << endl;
+        }
+        return allvalues;
+    }
+};
+// Initialize pointer to zero so that it can be initialized in first call to getInstance
+Mask2* Mask2::instance = nullptr;
 
 void part1(vector<string> slines) {
     
@@ -109,7 +197,7 @@ void part1(vector<string> slines) {
             auto match0 = reg0[0].match; //assume is a nice string
 
             // set the current mask
-            the_mask->setMask(match0); //error
+            the_mask->setMask(match0);
             //cout << "New mask set" << endl;
 
         }
@@ -156,6 +244,78 @@ void part2(vector<string> slines) {
     //bitmask acts on the memory address, not on the value
 
     //X is floating, can take any value
+
+    Mask2* the_mask2 = Mask2::getInstance();
+
+    //implement the memory as a hasmap
+    unordered_map <uintll, uintll> memory;
+
+    for (auto s : slines) {
+        if (s.size() < 0) {
+            continue; //skip line
+        }
+        auto s0 = s.substr(0, 3);
+
+
+        if (s0 == "mas") {
+            //mask
+            string pat0 = "[X10]{36}";
+
+            auto reg0 = regex_get_matches_and_their_groups(s, pat0);
+
+            if (reg0.size() != 1) {
+                cerr << "Error getting binary mask with line:" << s0 << endl;
+                continue;
+            }
+
+            auto match0 = reg0[0].match; //assume is a nice string
+
+            // set the current mask
+            the_mask2->setMask2(match0);
+            //cout << "New mask set" << endl;
+
+        }
+
+        else if (s0 == "mem") {
+            string pat0 = "mem\\[([0-9]+)\\]\\s*=\\s*([0-9]+)";
+            auto reg0 = regex_get_matches_and_their_groups(s, pat0);
+
+            //expected single match with 2 groups
+            if (reg0.size() != 1) {
+                if (reg0[0].groups.size() != 2) {
+                    cerr << "Error getting binary mask with line:" << s0 << endl;
+                    continue;
+                }
+            }
+            auto mem_pos = stoll(reg0[0].groups[0]);
+            uintll new_value = stoll(reg0[0].groups[1]);
+
+            cout << "mem position (before mask): " << mem_pos << " , value: " << new_value <<endl;
+
+            //mask mem position, to get several postitions
+            auto masked_values = the_mask2->applyMaskTo(mem_pos);
+            
+            //Apply new value to all new memory positions
+            cout << "Applying new value to all new masked positions" << endl;
+            //cout << "value " << new_value << " set at positions ";
+            for (const auto& mv0 : masked_values) {
+                memory[mv0] = new_value;
+                //cout << mv0 << ", ";
+            }
+            cout << endl;
+
+        }
+
+    }
+
+    //Get the sum
+    uintll sum = 0;
+
+    for (const auto& m : memory) {
+        sum += m.second;
+    }
+
+    cout << "sum is : " << sum << endl;
 }
 
 
@@ -164,31 +324,18 @@ int main() {
     //Read data from file to list of strings
 
     //auto slines = get_lines_from_file("input_test.txt");
+    //auto slines = get_lines_from_file("input.txt");
+    //part1(slines);
 
-    auto slines = get_lines_from_file("input.txt"); // 1535242681 not correct,
 
-    part1(slines);
+    //auto slines = get_lines_from_file("input_test_part2.txt");
+    //auto slines = get_lines_from_file("input_test_part2b.txt");
+    auto slines = get_lines_from_file("input.txt"); // 2847251749640 answer too low
+    // 3885232834169
+    part2(slines);
 
-    // part2(slines);
+    // too low
 
     return 0;
 }
 
-
-/*
-*
-* I am getting wrong results when using the chinese remainder theorem
-*
-
-
-nums: 13, 41, 641, 19, 17, 29, 661, 37, 23,
-rems: 67, 64, 54, 42, 37, 25, 23, 17, 0,
-
-results value 1063858958135574
-(note that the result here is the crt() - 67 )
-
-but sympy crt function gives
-800177252346225
-which is the correct value
-
-*/
